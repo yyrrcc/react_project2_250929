270b45559b4f241ae2c90a2972336abb49374c7f
yyrrcc <music_v@naver.com>
2025-09-29 14:16:04 +0900

    검색 기능 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import "./css/TodoList.css";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todo }) => {
+  // 0. 검색 기능 만들기 위해 useState 훅, onChange 함수 이용하기
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // 1. 검색어 필터링 함수
+  // 2. 대소문자 구별하지 않게 하기 위해 .toLowerCase() 메서드 추가
+  const getSearchResult = () => {
+    return search === "" ? todo : todo.filter((it) => it.content.toLowerCase().includes(search.toLocaleLowerCase()));
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input className="searchbar" value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
       <div className="list_wrapper">
-        {/* TodoItem을 반복해서 값 찍어주기 (배열 데이터를 렌더링할 때는 배열 메서드 map을 주로 이용)
+        {/* 0. TodoItem을 반복해서 값 찍어주기 (배열 데이터를 렌더링할 때는 배열 메서드 map을 주로 이용)
         HTML 으로 찍을 수도 있고 <div>{it.content}</div>, 컴포넌트로 반환 할 수도 있다
         it = {id, isDone, content, createDate} .. 을 의미 -> props로 넘겨주기
         아이템마다 가지고 있는 고유 key 값이 필요함 = 미리 만들어 뒀던 id 값으로 이용*/}
-        {todo.map((it) => (
+
+        {/* {todo.map((it) => (
+          <TodoItem key={it.id} {...it} />
+        ))} */}
+
+        {/* 1. 검색어 필터링 함수를 통해서 todo의 값을 가져오기 */}
+        {getSearchResult().map((it) => (
           <TodoItem key={it.id} {...it} />
         ))}
       </div>
